dd0668be6fb44d94ec4a7609ff6c34716bbbca18
Kian-F <52603628+Kian-F@users.noreply.github.com>
2021-01-04 23:06:54 +1100

    dispatch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
-import React from 'react'
-import {useSelector} from 'react-redux'
+import React from 'react';
+import {useSelector} from 'react-redux';
+import {increment} from './actions'
 
 function App() {
   const counter = useSelector(state => state.counter)
+ const isLogged = useSelector(state => state.isLogged)
+ const dispatch = useDispatch()
   return (
     <div className="App">
       <p>{counter}</p>
+      <button onClick={()=> dispatch(increment())}>+</button>
+      <button>-</button>
     </div>
   );
 }
